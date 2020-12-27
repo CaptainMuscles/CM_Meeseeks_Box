972b237aae68eabca54ea284a47abdbe0eb49917
NathanHarbour <nathanharbour@gmail.com>
2020-12-27 16:38:35 -0600

    Restrict available mental breaks

---
+++ Source/CM_Meeseeks_Box/Patches/MentalBreakWorkerPatches.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace CM_Meeseeks_Box
+{
+    [StaticConstructorOnStartup]
+    public static class MentalBreakWorkerPatches
+    {
+        [HarmonyPatch(typeof(MentalBreakWorker))]
+        [HarmonyPatch("BreakCanOccur", MethodType.Normal)]
+        public static class MentalBreakWorker_BreakCanOccur
+        {
+            private static List<string> disallowedMentalBreakNames = new List<string>
+                { "FireStartingSpree", "Binging_DrugExtreme", "Jailbreaker", "Slaughterer", "RunWild", "GiveUpExit",
+                  "Binging_DrugMajor", "BedroomTantrum", "SadisticRage",
+                  "Binging_Food", "Wander_OwnRoom"};
+
+            [HarmonyPostfix]
+            public static void Postfix(MentalBreakWorker __instance, ref bool __result, Pawn pawn)
+            {
+                if (__result == true && pawn.GetComp<CompMeeseeksMemory>() != null && disallowedMentalBreakNames.Contains(__instance.def.defName))
+                {
+                    __result = false;
+                }
+            }
+        }
+    }
+}
