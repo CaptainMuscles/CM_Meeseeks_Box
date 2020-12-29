f58c2f22c5dbf761063e4468ce41497d6005dd39
NathanHarbour <nathanharbour@gmail.com>
2020-12-29 16:40:06 -0600

    Fixed job cells being added to target list in non-construction jobs

--- Source/CM_Meeseeks_Box/CompMeeseeksMemory.cs
+++ Source/CM_Meeseeks_Box/CompMeeseeksMemory.cs
@@ -162,7 +162,7 @@ namespace CM_Meeseeks_Box
         public void AddJobTarget(LocalTargetInfo target, WorkGiverDef workGiverDef)
         {
             // Construction jobs need to track the cell in order to advance to the next stage when searching for jobs later
-            if (target.HasThing && workGiverDef != null && WorkerDefUtility.GetCombinedDefs(workGiverDef).Contains(workGiverDef))
+            if (target.HasThing && workGiverDef != null && WorkerDefUtility.constructionDefs.Contains(workGiverDef))
             {
                 AddJobTarget(target.Cell);
             }
