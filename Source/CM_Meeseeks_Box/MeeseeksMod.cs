0c311ae6e28392c41bee6f75553aeae0719aee09
NathanHarbour <nathanharbour@gmail.com>
2021-01-06 13:41:48 -0600

    Empty Modsettings

--- Source/CM_Meeseeks_Box/MeeseeksMod.cs
+++ Source/CM_Meeseeks_Box/MeeseeksMod.cs
@@ -1,4 +1,5 @@
-﻿using HarmonyLib;
+﻿using UnityEngine;
+using HarmonyLib;
 using RimWorld;
 using Verse;
 
@@ -9,12 +10,36 @@ namespace CM_Meeseeks_Box
         private static MeeseeksMod _instance;
         public static MeeseeksMod Instance => _instance;
 
+        public static MeeseeksModSettings settings;
+
+        public override string SettingsCategory()
+        {
+            return "Meeseeks Box";
+        }
+
         public MeeseeksMod(ModContentPack content) : base(content)
         {
             var harmony = new Harmony("CM_Meeseeks_Box");
             harmony.PatchAll();
 
             _instance = this;
+
+            settings = GetSettings<MeeseeksModSettings>();
         }
+
+        //public override void DoSettingsWindowContents(Rect inRect)
+        //{
+        //    base.DoSettingsWindowContents(inRect);
+        //    Listing_Standard listing_Standard = new Listing_Standard();
+        //    listing_Standard.Begin(inRect);
+        //    listing_Standard.CheckboxLabeled("CM_Meeseeks_Box_MsMeeseeksSettingLabel", ref settings.msMeeseeks, "CM_Meeseeks_Box_MsMeeseeksSettingTooltip");
+        //    listing_Standard.End();
+        //}
+
+        //public override void WriteSettings()
+        //{
+        //    base.WriteSettings();
+        //    settings.UpdateSettings();
+        //}
     }
 }
---
+++ Source/CM_Meeseeks_Box/MeeseeksModSettings.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace CM_Meeseeks_Box
+{
+    public class MeeseeksModSettings : ModSettings
+    {
+        public bool msMeeseeks = false;
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+
+            Scribe_Values.Look(ref msMeeseeks, "msMeeseeks", false);
+        }
+
+        public void UpdateSettings()
+        {
+            if (msMeeseeks)
+            {
+
+            }
+        }
+    }
+}
